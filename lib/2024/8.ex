46668006c8ad84a12d5308f3ffdc21a7d8de2eeb
Juergen Braungardt <Juergen.Braungardt@mevis.de>
2024-12-11 16:14:00 +0100

    day 8 p2

--- lib/2024/8.ex
+++ lib/2024/8.ex
@@ -15,7 +15,7 @@ aoc 2024, 8 do
     |> Enum.filter(fn {_, _, char} -> char != "." end)
     |> Enum.group_by(fn {_, _, char} -> char end, fn {row, col, _} -> {row, col} end)
     |> Enum.map(fn {_, nodes} ->
-      antinodes(nodes, [])
+      antinodes(nodes, [], cols, rows)
       |> Enum.filter(fn {x, y} -> x >= 0 && y >= 0 && x < rows && y < cols end)
     end)
     |> List.flatten()
@@ -23,48 +23,69 @@ aoc 2024, 8 do
     |> Enum.count()
   end
 
-  defp antinodes([_last], antinodes), do: antinodes
+  defp antinodes([_last], antinodes, _width, _height), do: antinodes
 
-  defp antinodes([{hx, hy} | t], antinodes) do
+  defp antinodes([{hx, hy} | t], antinodes, width, height) do
     antinodes =
       Enum.reduce(t, antinodes, fn {nx, ny}, acc ->
-        {dx, dy} = {
-          (nx - hx) |> abs(),
-          (ny - hy) |> abs()
-        }
-
         acc
-        |> List.insert_at(
-          0,
-          {cond do
-             nx >= hx -> nx + dx
-             nx < hx -> nx - dx
-           end,
-           cond do
-             ny >= hy -> ny + dy
-             ny < hy -> ny - dy
-           end}
-        )
-        |> List.insert_at(
-          0,
-          {cond do
-             hx >= nx -> hx + dx
-             hx < nx -> hx - dx
-           end,
-           cond do
-             hy >= ny -> hy + dy
-             hy < ny -> hy - dy
-           end}
-        )
+        |> add_node({nx, ny}, {nx - hx, ny - hy}, width, height)
+        |> add_node({hx, hy}, {hx - nx, hy - ny}, width, height)
       end)
 
-    antinodes(t, antinodes)
+    antinodes(t, antinodes, width, height)
+  end
+
+  defp add_node(nodes, {nx, ny}, {dx, dy}, width, height)
+       when nx + dx < 0 or nx + dx > width or ny + dy < 0 or ny + dy > height,
+       do: nodes
+
+  defp add_node(nodes, {nx, ny}, {dx, dy}, _width, _height) do
+    List.insert_at(nodes, 0, {nx + dx, ny + dy})
   end
 
   @doc """
       iex> p2(example_string())
   """
-  def p2(_input) do
+  def p2(input) do
+    {rows, cols, grid} = parse_input(input)
+
+    grid
+    |> Enum.filter(fn {_, _, char} -> char != "." end)
+    |> Enum.group_by(fn {_, _, char} -> char end, fn {row, col, _} -> {row, col} end)
+    |> Enum.map(fn {_, nodes} ->
+      antinodes2(nodes, [], cols, rows)
+      |> Enum.filter(fn {x, y} -> x >= 0 && y >= 0 && x < rows && y < cols end)
+    end)
+    |> List.flatten()
+    |> Enum.uniq()
+    |> Enum.count()
+  end
+
+  defp antinodes2([_last], antinodes, _width, _height), do: antinodes
+
+  defp antinodes2([{hx, hy} | t], antinodes, width, height) do
+    antinodes =
+      Enum.reduce(t, antinodes, fn {nx, ny}, acc ->
+        acc
+        |> List.insert_at(0, {nx, ny})
+        |> List.insert_at(0, {hx, hy})
+        |> add_node2({nx, ny}, {nx - hx, ny - hy}, width, height)
+        |> add_node2({hx, hy}, {hx - nx, hy - ny}, width, height)
+      end)
+
+    antinodes2(t, antinodes, width, height)
+  end
+
+  defp add_node2(nodes, {nx, ny}, {dx, dy}, width, height)
+       when nx + dx < 0 or nx + dx > width or ny + dy < 0 or ny + dy > height,
+       do: nodes
+
+  defp add_node2(nodes, {nx, ny}, {dx, dy}, width, height) do
+    new_node = {nx + dx, ny + dy}
+
+    List.insert_at(nodes, 0, new_node)
+    |> add_node2(new_node, {dx, dy}, width, height)
   end
 
   defp parse_input(input) do
--- test/2024/8_test.exs
+++ test/2024/8_test.exs
@@ -2,6 +2,10 @@ import AOC
 
 aoc_test 2024, 8, async: true do
   test "p1" do
-    assert Y2024.D8.p1(example_string()) == 14
+    assert p1(example_string()) == 14
+  end
+
+  test "p2" do
+    assert p2(example_string()) == 34
   end
 end
